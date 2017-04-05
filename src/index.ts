f25dec29b214f57c54a30234f9ecf54e12ca4def
entson <dunnaugh@gmail.com>
2017-04-05 15:17:02 -0700

    add to csv, fix essential grammar

--- src/index.ts
+++ src/index.ts
@@ -16,12 +16,13 @@ readFile("duck_sized.csv", {encoding: "utf-8"}, function(err,data){
     }
     var rand1 = Math.floor(Math.random() * data.length);
     var rand2 = Math.floor(Math.random() * data.length);
-    var animal2 = data[rand2][0];
     var animal1 = data[rand1][0];
     
     if (data[rand2][1] = 'small') {
-      animal2 = Math.floor(Math.random() * 5) + ' ' + data[rand2][0] + 's';
+      var count = Math.floor(Math.random() * 5)+1
+      var animal2 = data[rand2][0];
     } else {
+      count = " a "
       animal2 = data[rand2][0];
     }
 
@@ -30,7 +31,7 @@ readFile("duck_sized.csv", {encoding: "utf-8"}, function(err,data){
     console.log(rand2);
     console.log(animal1);
     console.log(animal2);
-    console.log("Would you rather fight a " + animal1 + "-sized " + animal2 + ", or a " + animal2 + "-sized " + animal1+ "?");
+    console.log("Would you rather fight a " + animal1 + "-sized " + animal2 + ", or " + count + " " + animal2 + "-sized " + animal1+ "s?");
   });
 });
 
