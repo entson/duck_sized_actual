9d77f98de99fa0fc31eb229993c0aea1c7d97642
entson <dunnaugh@gmail.com>
2017-04-05 15:39:54 -0700

    actually fixed

--- src/index.ts
+++ src/index.ts
@@ -17,20 +17,15 @@ readFile("duck_sized.csv", {encoding: "utf-8"}, function(err,data){
     var rand1 = Math.floor(Math.random() * data.length);
     var rand2 = Math.floor(Math.random() * data.length);
     var animal1 = data[rand1][0];
-    
+    var count;
+
     if (data[rand2][1] = 'small') {
-      var count = Math.floor(Math.random() * 5)+1
+      count = Math.floor(Math.random() * 5).toString()
       var animal2 = data[rand2][0];
     } else {
       count = " a "
       animal2 = data[rand2][0];
     }
-
-    console.log(data);
-    console.log(rand1);
-    console.log(rand2);
-    console.log(animal1);
-    console.log(animal2);
     console.log("Would you rather fight a " + animal1 + "-sized " + animal2 + ", or " + count + " " + animal2 + "-sized " + animal1+ "s?");
   });
 });
